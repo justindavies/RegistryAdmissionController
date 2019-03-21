2f1ad767787cf02a4c1c76a4595dd57c12410ad2
Justin Davies <juda@microsoft.com>
2019-03-21 15:33:35 +0000

    Initial Commit

---
+++ controller/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+const https = require('https')
+const fs = require('fs')
+
+
+app.use(express.json());
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  var admissionRequest = req.body;
+  console.log(admissionRequest);
+  // Get a reference to the pod spec
+  var object = admissionRequest.request.object;
+
+  console.log(`validating the ${object.metadata.name} pod`);
+
+  var admissionResponse = {
+    allowed: false
+  };
+
+  var found = false;
+  for (var container of object.spec.containers) {
+    if (container.image.includes(process.env.CONTAINER_REGISTRY) === false) {
+      console.log(`${container.name} is not using the allowed registry`);
+
+      admissionResponse.status = {
+        status: 'Failure',
+        message: `${container.name} is not using the allowed registry`,
+        reason: `${container.name} is not using the allowed registry`,
+        code: 402
+      };
+
+      found = true;
+    };
+  };
+
+  if (!found) {
+    admissionResponse.allowed = true;
+  }
+
+  var admissionReview = {
+    response: admissionResponse
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(admissionReview));
+  res.status(200).end();
+});
+
+
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}, app).listen(3000, () => {
+  console.log('Listening...')
+})
+
+
+
